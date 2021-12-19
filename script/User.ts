cab0646f83f43a8a45b89f3b76ef9bf5a5917bcb
mac <mac@macdeMacBook-Pro.local>
2021-12-19 14:30:09 +0800

    add constructor

--- script/User.ts
+++ script/User.ts
@@ -1,4 +1,11 @@
-class User{
+import faker from 'faker';
+// type definition
+
+// never use default statement
+// export default 'red';
+
+
+export class User{
     name: string;
     location:{
         lat: number;
@@ -7,6 +14,11 @@ class User{
 
     // faker npmjs
     constructor(){
-
+        this.name = faker.name.firstName();
+        console.log(this.location);
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        };
     }
 }
---
+++ script/company.ts
@@ -0,0 +1,19 @@
+import faker from 'faker'
+
+export class Company{
+    companyName: string;
+    catchPhrase: string;
+    location:{
+        lat: number;
+        lng: number;
+    }
+
+    cosntructor(){
+        this.companyName = faker.company.companyName();
+        this.catchPhrase = faker.company.catchPhrase();
+        this.location = {
+            lat: parseFloat(faker.address.lattitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
+    }
+}
